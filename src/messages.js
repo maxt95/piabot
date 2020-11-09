19f5bd5c06fa1e1eaaaf120244c5bb2480180b49
Max Conrad <maxconrad@me.com>
2020-11-08 21:40:19 -0500

    change to setting role instead of adding and removing per rate limit

--- src/messages.js
+++ src/messages.js
@@ -1,17 +1,9 @@
 import { Command } from './db/schema.js'
 
-const addRoles = async (user, roles) => {
+const setRoles = async (user, existingRoles, newRoles) => {
+  const roles = existingRoles.concat(newRoles)
   try {
-    console.log('adding roles: ', roles)
-    await user.roles.add(roles)
-  } catch(error) {
-    console.log(error)
-  }
-}
-
-const removeRoles = async (user, roles) => {
-  try {
-    await user.roles.remove(roles)
+    await user.roles.set(roles)
   } catch(error) {
     console.log(error)
   }
@@ -30,15 +22,15 @@ const messageHandler = (client) => {
         commandPhrase = commandPhrase.split(' ')
         const command = commandPhrase[0] + ' ' + commandPhrase[1]
         const user = message.mentions.members.first()
-
+        
         try {
           const existingCommands = await Command.find({guild: guild.id}).exec()
 
           existingCommands.forEach((com) => {
             if(command === com.command) {
               if (com.deletable === true) message.delete()
-              if (com.addRoles.length > 0) addRoles(user, com.addRoles)
-              if (com.removeRoles.length > 0) removeRoles(user, com.removeRoles)
+              const existingRoles = Array.from(user.roles.cache.filter(role => !role.id.includes(com.removeRoles)).values())
+              setRoles(user, existingRoles, com.addRoles)
               if (com.botResponse !== '')  {
                 let response = com.botResponse
                 response = response.replace("<user>", `<@${user.id}>`)
